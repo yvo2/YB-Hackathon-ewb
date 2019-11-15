4d71e3322016130281ccaf7379d6f0685c9312a8
tabstma <marco.2000@icloud.com>
2019-11-15 16:24:52 +0100

    cleanup

--- src/preload.js
+++ src/preload.js
@@ -1,13 +1,11 @@
 import cityTestMap from './assets/maps/test-city1.csv';
 import rogueLikeCity from './assets/tilesets/roguelikeCity_magenta.png'
-import mario_tiles from './assets/tilesets/mario_tiles.png'
 import human from './assets/sprites/human/SMB_Base_M_Tone08.png'
 
 
 export default function() {
   this.load.image("city-tiles", rogueLikeCity);
   this.load.tilemapCSV("city-map", cityTestMap);
-  this.load.image("mario-tiles", mario_tiles);
   this.load.spritesheet('human', human, {
     frameWidth: 55,
     frameHeight: 55,
