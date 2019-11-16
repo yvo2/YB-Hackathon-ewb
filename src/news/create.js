774261c59c34b469c4e4edf00ec2d90ec1b42276
tabstma <marco.2000@icloud.com>
2019-11-16 07:44:59 +0100

    WIP

--- src/news/create.js
+++ src/news/create.js
@@ -1,6 +1,10 @@
+import createSpeechBubble from '../createSpeechBubble';
+import office from '../office';
+
 export default function() {
-  this.news = this.physics.add.sprite(450, 300, "news");
-  this.news.setScale(6)  
+  this.news = this.physics.add.sprite(575, 300, "news");
+  this.news.setScale(3)  
+  this.game.objective = ""
 
   const createAnim = (sprite, key, frames) => {
     this.anims.create({
@@ -14,11 +18,22 @@ export default function() {
   createAnim("news", "anime", [1,1,0,1,0,0]);
 
   this.news.anims.play('anime');
+  let newsText;
+  if(this.game.summary.score > 100) {
+    newsText = "GUT:\nHeute Wurden im Däu-Hölzli 26 Löwen-Babys geboren\nHeute gab es seit 10 Jahren wieder den ersten Schneefall in der Schweiz, und zwar auf dem Matterhorn."
+    this.physics.add.image(100, 100, "good")
+  } else {
+    newsText = "Heute sind Amurleoparde offiziell ausgestorben. Sie sind an der extremen Hitze von bis zu 70 Grad von der Erde verschwunden.\nIn der Wüste Dascht e Lut in Iran ist Heute eine neue Rekordtemparatur von 115 Grad gemessen worden."
+    this.physics.add.image(100, 100, "bad")
+  }
+
+  createSpeechBubble(575, 300, newsText, this)
 
   this.input.keyboard.on('keydown', () => {
     //ACTION
     if(event.code === "Space") {
-
+      game.scene.add('office', office, true)
+      game.scene.remove('news')
     }
   });
 }
--- src/news/preload.js
+++ src/news/preload.js
@@ -1,9 +1,15 @@
 import news from '../assets/sprites/news.png';
+import good from '../assets/good.png';
+import bad from '../assets/bad.png';
+
 
 export default function() {
+  this.load.image("good", good);
+  this.load.image("bad", bad);
+  
   this.load.spritesheet('news', news, {
-    frameWidth: 150,
-    frameHeight: 100,
+    frameWidth: 300,
+    frameHeight: 200,
     startFrame: 0,
     endFrame: 2
   });
