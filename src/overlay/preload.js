0f78c3956415bc35d7a80437573491f64248cbff
tabstma <marco.2000@icloud.com>
2019-11-16 08:14:49 +0100

    added background for HUD

--- src/overlay/preload.js
+++ src/overlay/preload.js
@@ -2,8 +2,12 @@ import music_mp3 from "../assets/music/bg_music.mp3";
 import music_ogg from "../assets/music/bg_music.ogg";
 
 import bell from '../assets/sprites/car.png';
+import pointB from '../assets/pointBackground.png';
+import questB from '../assets/questBackground.png';
 
 export default function() {
+  this.load.image("questB", questB);
+  this.load.image("pointB", pointB);
   this.load.audio('music', [music_mp3, music_ogg]);
   this.load.spritesheet('bell', bell, {
     frameWidth: 30,
@@ -11,6 +15,7 @@ export default function() {
     startFrame: 0,
     endFrame: 2
   });
+  
 }
 
 
