ec2b4950c643b19bcc5048a5b6b13bc845300c57
tabstma <marco.2000@icloud.com>
2019-11-16 07:58:00 +0100

    right images & finished cutscene

--- src/news/create.js
+++ src/news/create.js
@@ -21,10 +21,10 @@ export default function() {
   let newsText;
   if(this.game.summary.score > 100) {
     newsText = "GUT:\nHeute Wurden im Däu-Hölzli 26 Löwen-Babys geboren\nHeute gab es seit 10 Jahren wieder den ersten Schneefall in der Schweiz, und zwar auf dem Matterhorn."
-    this.physics.add.image(100, 100, "good")
+    this.physics.add.image(367, 162, "good")
   } else {
     newsText = "Heute sind Amurleoparde offiziell ausgestorben. Sie sind an der extremen Hitze von bis zu 70 Grad von der Erde verschwunden.\nIn der Wüste Dascht e Lut in Iran ist Heute eine neue Rekordtemparatur von 115 Grad gemessen worden."
-    this.physics.add.image(100, 100, "bad")
+    this.physics.add.image(367, 162, "bad")
   }
 
   createSpeechBubble(575, 300, newsText, this, true)
